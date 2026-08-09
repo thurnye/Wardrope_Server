e7c31765b31b623eed86e9989403fb4f79acda4f
thurnye <danthurnye@gmail.com>
2026-08-09 11:34:43 -0400

    Add fragrance persistence model

---
+++ src/Wardrope.DB/models/Fragrance/fragrance.model.ts
@@ -0,0 +1,39 @@
+import type { ObjectId } from 'mongodb';
+import type { FragranceConcentration } from '../../../Wardrope.Core/Models/Fragrance/fragrance.model';
+
+export const FRAGRANCES_COLLECTION = 'fragrances';
+
+export interface FragranceImageDocument {
+  objectKey: string;
+  etag: string | null;
+  contentType: 'image/webp';
+  width: number;
+  height: number;
+  sizeBytes: number;
+  updatedAt: Date;
+}
+
+export interface FragrancePurchasePriceDocument {
+  amount: number;
+  currency: string;
+}
+
+export interface FragranceDocument {
+  _id: ObjectId;
+  userId: ObjectId;
+  brand: string;
+  name: string;
+  productLine: string | null;
+  concentration: FragranceConcentration | null;
+  fragranceFamily: string | null;
+  scentType: string | null;
+  keyNotes: string[];
+  bottleSizeMl: number | null;
+  amountRemainingPercent: number | null;
+  purchaseDate: string | null;
+  purchasePrice: FragrancePurchasePriceDocument | null;
+  available: boolean;
+  image: FragranceImageDocument | null;
+  createdAt: Date;
+  updatedAt: Date;
+}
